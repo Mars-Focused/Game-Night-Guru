939d497161ef67005f22b77600e37fc9361c8120
Mars_Clarke <marsthecoder@outlook.com>
2020-12-07 10:16:48 -0700

    10:16am monday

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
-import './App.css';
-import Header from './Components/Header.js'
-import AddGame from './Components/AddGame'
-import GameList from './Components/GameList'
-import Axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Components/Header.js";
+import AddGame from "./Components/AddGame";
+import GameList from "./Components/GameList";
+import Axios from "axios";
 
 /*
 Update score needs to be written here
@@ -18,35 +18,33 @@ and can hold state
 
 export default class App extends Component {
   constructor() {
-    super()
-    this.state ={
+    super();
+    this.state = {
       gamesArr: [],
-      userInput: ''
-    }
+      userInput: "",
+    };
     //this is how we give explicit content to this
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.removeGame = this.removeGame.bind(this)
-    this.upvoteGame = this.upvoteGame.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.removeGame = this.removeGame.bind(this);
+    this.upvoteGame = this.upvoteGame.bind(this);
   }
 
-// This is how I got information from the back end
+  // This is how I got information from the back end
   componentDidMount() {
-    Axios
-      .get('/api/games')
-      .then(res =>{
-        // console.log(res.data)
-        this.setState({gamesArr: res.data})
-      })
+    Axios.get("/api/games").then((res) => {
+      // console.log(res.data)
+      this.setState({ gamesArr: res.data });
+    });
   }
 
   handleInputChange(e) {
-    console.log(e.target.value)
+    console.log(e.target.value);
     this.setState({
-      userInput: e.target.value
-    
-    })
+      userInput: e.target.value,
+    });
   }
 
+  //this needs to be changed to add e.target.value
   addGame() {
     // Axios
     // .create(`/api/game/${UserInput}`)
@@ -56,35 +54,34 @@ export default class App extends Component {
   }
 
   upvoteGame(id) {
-    Axios
-    .put(`/api/games/${id}`)
-    .then(res =>{
-      this.setState({gamesArr: res.data})
-    })
+    Axios.put(`/api/games/${id}`).then((res) => {
+      this.setState({ gamesArr: res.data });
+    });
   }
 
   removeGame(id) {
-    Axios
-    .delete(`/api/games/${id}`)
-    .then(res =>{
-      this.setState({gamesArr: res.data})
-    })
+    Axios.delete(`/api/games/${id}`).then((res) => {
+      this.setState({ gamesArr: res.data });
+    });
   }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <input onChange={this.handleInputChange}/>
-        <AddGame 
-        addGame = { this.addGame }
-        userInput ={ this.state.userInput }
-        testProp = 'test'/>
+        {/* this is my extra limb and I need to get rid of it / move it. */}
+        <input onChange={this.handleInputChange} />
+        <AddGame
+          addGame={this.addGame}
+          userInput={this.state.userInput}
+          testProp="test"
+        />
 
         <GameList
-        upvoteGame = { this.upvoteGame }
-        removeGame = { this.removeGame }
-        gamesArr = { this.state.gamesArr }/>
+          upvoteGame={this.upvoteGame}
+          removeGame={this.removeGame}
+          gamesArr={this.state.gamesArr}
+        />
       </div>
     );
   }
